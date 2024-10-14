054c516b2ffa570d17b8c824b5628539d5741827
kareem <kareemwally@std.mans.edu.eg>
2024-10-14 22:21:59 +0300

    type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     the many times we use the `wait_random` (n)
     """
