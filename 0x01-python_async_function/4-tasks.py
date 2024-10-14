bf8648d921f0ca8e48a076cedb1095c23c2467d2
kareem <kareemwally@std.mans.edu.eg>
2024-10-14 23:09:44 +0300

    tasks many times

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,10 +4,10 @@ A module to run multiple async tasks concurrently.
 """
 import asyncio
 from typing import List
-from 3-tasks import task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Spawns task_wait_random n times with a max_delay.
 
