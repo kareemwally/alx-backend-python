6ec52afe971c05052cd6f4c6757aeaccba38b181
kareem <kareemwally@std.mans.edu.eg>
2024-10-14 21:21:24 +0300

    basix delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+a simple module with a function to generate a random numbee.
+from 0:10 async
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """
+    an asyncrounous function to return a random integer between0:10
+    """
+    res = random.uniform(0, max_delay)
+    await asyncio.sleep(res)
+    return res
