ccb1f6c2d03ced23c29700ddbb899d7cc3a2f74e
kareem <kareemwally@std.mans.edu.eg>
2024-10-14 22:15:16 +0300

    n times async delays

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+implementing the wait_random function for many times n
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> float:
+    """
+    the many times we use the `wait_random` (n)
+    """
+    return [await wait_random(max_delay) for _ in range(n)]
