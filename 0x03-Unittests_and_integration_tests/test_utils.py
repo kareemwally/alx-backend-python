62f2c2efa5ff3a907d73a9f62c17cffc224ea73c
kareem <kareemwally@std.mans.edu.eg>
2024-11-04 21:34:52 +0200

    json testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,7 +34,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({}, ("a",), KeyError, "a"),
         ({"a": 1}, ("a", "b"), KeyError, "b"),
-    ])
+        ])
     def test_access_nested_map_exception(self,
                                          nested_map: Mapping[str, Any],
                                          path: Sequence[str],
@@ -44,6 +44,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaisesRegex(KeyError, expected_exception_message):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test class for utils.get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test that utils.get_json returns expected result and
+        requests.get is called once with correct url
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
