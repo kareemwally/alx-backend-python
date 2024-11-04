aee40f6343b50522c25b536e99c42c260ec722c1
kareem <kareemwally@std.mans.edu.eg>
2024-11-04 19:03:26 +0200

    testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python3
 """
-simple module contain a test class inhereted from
-unittest.TestCase
+simple module to test functions in utils.py
+using unittest module
 """
 import unittest
-from utils import access_nested_map
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any, Union
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    testing the access_nested_map function from utils
+    Testclass inherents the unittest.TestCase module
     """
     @parameterized.expand([
-                          ({"a": 1}, ("a",), 1),
-                          ({"a": {"b": 2}}, ("a",), {"b": 2}),
-                          ({"a": {"b": 2}}, ("a", "b"), 2),
-                          ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
-                          ({}, ("a",), KeyError),
-                          ({"a": 1}, ("a", "b"), KeyError),
-                          ({"a": {"b": 2}}, ("a", "b", "c"), KeyError),
-                          ])
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
+    ])
     def test_access_nested_map(self,
                                nested_map: Mapping[str, Any],
                                path: Sequence[str],
-                               expected_result: Union[Any, type(Exception)]):
+                               expected_result: Any):
         """
-        asserting the testcases from @parameterized.expand
-        into function
+        teting the access_nested_map
         """
-        if isinstance(expected_result, type) and issubclass(expected_result,
-                                                            Exception):
-            with self.assertRaises(expected_result):
-                access_nested_map(nested_map, path)
-        else:
-            self.assertEqual(access_nested_map(nested_map, path),
-                             expected_result)
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "key 'a' not found in {}"),
+        ({"a": 1}, ("a", "b"), "key 'b' not found in {'a': 1}"),
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping[str, Any],
+                                         path: Sequence[str],
+                                         expected_exception_message: str):
+        """
+        testing access_nested_map_exception
+        """
+        with self.assertRaisesRegex(KeyError, expected_exception_message):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == '__main__':
