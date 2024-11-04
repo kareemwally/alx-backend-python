82f30fa29ccc8e311338363a51b07ff2ebf74210
kareem <kareemwally@std.mans.edu.eg>
2024-11-04 17:29:59 +0200

    more testcases

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,20 +12,31 @@ class TestAccessNestedMap(unittest.TestCase):
     testing the access_nested_map function from utils
     """
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ({"a": {"b": {"c": 2}}}, ("a", "b", "c"), 2),
-        ({}, ("a"), KeyError),
-        ({"a": {"b": 2}}, ("a", "c"), KeyError),
-
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+                          ({"a": 1}, ("a",), 1),
+                          ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                          ({"a": {"b": 2}}, ("a", "b"), 2),
+                          ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
+                          ({}, ("a",), KeyError),
+                          ({"a": 1}, ("a", "b"), KeyError),
+                          ({"a": {"b": 2}}, ("a", "b", "c"), KeyError),
+                          ])
+    def test_access_nested_map(self,
+                               nested_map: Mapping[str, Any],
+                               path: Sequence[str],
+                               expected_result: Union[Any, type(Exception)]):
         """
         asserting the testcases from @parameterized.expand
         into function
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        if isinstance(expected_result, type) and issubclass(expected_result,
+                                                            Exception):
+            with self.assertRaises(expected_result):
+                access_nested_map(nested_map, path)
+        else:
+            self.assertEqual(access_nested_map(nested_map, path),
+                             expected_result)
+        asserting the testcases from @parameterized.expand
+        into function
 
 
 if __name__ == '__main__':
